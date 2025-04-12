fa49c2d388649c32dc01ae7ed73bd3a3a6a365bd
qkb2 <jakubgr02@gmail.com>
2025-04-12 14:38:30 +0200

    change to mean filter, test on rpi3b

--- makefile
+++ makefile
@@ -1,5 +1,11 @@
-make:
+zad1:
 	gcc zad1.c -o zad1 -lm
+
+zad5:
+	gcc zad5.c -o zad5 -lm
+
+zad5qemu:
+	echo TODO: implement
 
 clean:
 	rm zad1
--- zad5.c
+++ zad5.c
@@ -1,6 +1,6 @@
 /*
 This program converts P6 PPM file to P5 PGM file. Works for 255 max values.
-Can be compiled normally with GCC without any flags or with makefile provided.
+Can be compiled normally with GCC for 64-bit ARM processors (tested for RPi3B).
 Used from cmd - first arg is source file name (opens as rb), second arg is target file name (opens as wb).
 By Jakub Grabowski
 */
@@ -54,22 +54,8 @@ void neon_mul_two_vectors(uint8_t* v1, uint8_t* v2, uint8_t* res) {
     vst1_u8(res, result);
 }
 
-uint8_t round_clamp(double x) {
-    double xm = round(x);
-    if (x > 255) return 255;
-    if (x < 0) return 0;
-    return (uint8_t)x;
-}
-
-uint8_t ppm_to_pgm_weighted(Pixel* pixel) {
-    // magic numbers
-    double wr = 0.299, wg = 0.587, wb = 0.114; // weights sum up to 1, no division necessary 
-    double wsum = wr * pixel->r + wg * pixel->g + wb * pixel->b;
-    return round_clamp(wsum);
-}
-
 void neon_weighted_grayscale(int size, Pixel* pixels, uint8_t* grayscale) {
-    // weights scaled to 8-bit fixed-point (approx.)
+    // weights magic numbers scaled to 8-bit fixed-point (approx.)
     const uint8x8_t wr = vdup_n_u8(77);   // 0.299 * 256 c. 77
     const uint8x8_t wg = vdup_n_u8(150);  // 0.587 * 256 c. 150
     const uint8x8_t wb = vdup_n_u8(29);   // 0.114 * 256 c. 29
@@ -94,33 +80,34 @@ void neon_weighted_grayscale(int size, Pixel* pixels, uint8_t* grayscale) {
     }
 }
 
-uint8_t get_safe_gval(int width, int height, int i, int j, uint8_t* grayscale) {
-    // to avoid darkening on the edges, return nearest actual pixel from the img
-    int ii = i, jj = j;
-    if (i < 0) ii = 0;
-    if (j < 0) jj = 0;
-    if (i >= width) ii = width - 1;
-    if (j >= height) jj = height - 1;
-    return grayscale[jj * width + ii];
-}
+void neon_mean_filter( int width, int height, uint8_t* grayscale, uint8_t* new_grayscale) {
+    for (int y = 1; y < height - 1; ++y) {
+        for (int x = 1; x < width - 1; ++x) {
+            // load 3×3 neighborhood manually
+            uint8_t a = grayscale[(y - 1) * width + (x - 1)];
+            uint8_t b = grayscale[(y - 1) * width + x];
+            uint8_t c = grayscale[(y - 1) * width + (x + 1)];
+            uint8_t d = grayscale[y * width + (x - 1)];
+            uint8_t e = grayscale[y * width + x];
+            uint8_t f = grayscale[y * width + (x + 1)];
+            uint8_t g = grayscale[(y + 1) * width + (x - 1)];
+            uint8_t h = grayscale[(y + 1) * width + x];
+            uint8_t i = grayscale[(y + 1) * width + (x + 1)];
 
-uint8_t* convolve_3x3(
-    int width, int height, uint8_t* grayscale, uint8_t* new_grayscale, double* kernel) {
-    for (int j = 0; j < height; j++) {
-        for (int i = 0; i < width; i++) {
-            double acc = 0;
-            for (int jj = 0; jj < KSIZE; jj++) {
-                for (int ii = 0; ii < KSIZE; ii++) {
-                    int ni = i + ii - 1;  // offset by kernel center
-                    int nj = j + jj - 1;
+            // load into NEON register: 8 elements
+            uint8x8_t vec = {a, b, c, d, e, f, g, h};
 
-                    double kval = kernel[jj * KSIZE + ii];
-                    uint8_t gval = get_safe_gval(width, height, ni, nj, grayscale);
+            // pairwise add and widen: gives 4 uint16 values (a+b, c+d, e+f, g+h)
+            uint16x4_t pair_sums = vpaddl_u8(vec);
 
-                    acc += kval * gval;
-                }
-            }
-            new_grayscale[j * width + i] = round_clamp(acc);
+            // sum all parts
+            uint16_t total = vget_lane_u16(pair_sums, 0) +
+                             vget_lane_u16(pair_sums, 1) +
+                             vget_lane_u16(pair_sums, 2) +
+                             vget_lane_u16(pair_sums, 3) + i;
+
+            uint8_t result = (uint8_t)(total / 9); // avg
+            new_grayscale[y * width + x] = result;
         }
     }
 }
@@ -215,12 +202,6 @@ int main(int argc, char const *argv[]) {
     neon_weighted_grayscale(size, pixels, grayscale);
     free(pixels);
 
-    // example approx. gaussian filter - can be changed to be any other 3x3 kernel
-    double kernel[KSIZE * KSIZE] = {
-        1.0 / 16, 2.0 / 16, 1.0 / 16,
-        2.0 / 16, 4.0 / 16, 2.0 / 16,
-        1.0 / 16, 2.0 / 16, 1.0 / 16
-    };
 
     uint8_t* new_grayscale = (uint8_t*)malloc(size);
     if (!new_grayscale) {
@@ -229,10 +210,11 @@ int main(int argc, char const *argv[]) {
         error_handler(src, tgt, "Memory allocation failed for grayscale data manipulation.");
     }
     
-    // neon_convolve_3x3(width, height, grayscale, new_grayscale, kernel);
+    neon_mean_filter(width, height, grayscale, new_grayscale);
+
+    fwrite(new_grayscale, sizeof(uint8_t), size, tgt);
 
     free(grayscale);
-    fwrite(new_grayscale, sizeof(uint8_t), size, tgt);
     free(new_grayscale);
 
     fclose(tgt);
